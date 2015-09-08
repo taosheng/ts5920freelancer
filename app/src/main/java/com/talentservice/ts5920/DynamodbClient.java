9d32f23dc2008f4cd8cbe24c86cd7eb371c6e81e
chen taosheng <dougchen@talent-service.com>
2015-09-08 17:02:06 +0800

    first commit

---
+++ app/src/main/java/com/talentservice/ts5920/DynamodbClient.java
@@ -0,0 +1,177 @@
+package com.talentservice.ts5920;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Iterator;
+import java.util.TimeZone;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.*;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBScanExpression;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBQueryExpression;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.PaginatedScanList;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ComparisonOperator;
+import com.amazonaws.services.dynamodbv2.model.Condition;
+
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBAttribute;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBRangeKey;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
+
+
+
+
+public class DynamodbClient{
+
+
+    static BasicAWSCredentials bac = new BasicAWSCredentials("NOSUCHKEY","NOSUCHAUTH");
+    static DynamoDBMapper mapper = new DynamoDBMapper(new AmazonDynamoDBClient(bac));
+
+	public static void println(Object msg){
+		System.out.println(msg);
+
+	}
+
+	public void modifyItem(TS5920Item item){
+		mapper.save(item);
+	}
+
+	public void new5920(String uuid, String target, String desc, String form) {
+	}
+
+	public boolean existingUID(String uid){
+
+		DynamoDBQueryExpression scanExpression = new DynamoDBQueryExpression();
+
+		Map<String, Condition> queryFilter = new HashMap<String, Condition>();
+
+		// Condition1: DeviceId
+		Condition scanCondition = new Condition()
+				.withComparisonOperator(ComparisonOperator.EQ.toString())
+				.withAttributeValueList(new AttributeValue().withS(uid));
+		queryFilter.put("uid", scanCondition);
+		scanExpression.setQueryFilter(queryFilter);
+
+		TS5920Item result = mapper.load(TS5920Item.class, uid);
+		if (result != null ){
+			return true;
+		}else{
+			return false;
+		}
+
+
+	}
+
+	public TS5920Item getItemByUid(String uid){
+		DynamoDBQueryExpression scanExpression = new DynamoDBQueryExpression();
+
+		Map<String, Condition> queryFilter = new HashMap<String, Condition>();
+
+		// Condition1: DeviceId
+		Condition scanCondition = new Condition()
+				.withComparisonOperator(ComparisonOperator.EQ.toString())
+				.withAttributeValueList(new AttributeValue().withS(uid));
+		queryFilter.put("uid", scanCondition);
+		scanExpression.setQueryFilter(queryFilter);
+        return mapper.load(TS5920Item.class,uid);
+		//List<TS5920Item> results = mapper.query(TS5920Item.class, scanExpression);
+		//if (results.size() > 0){
+		//	return results.get(0);
+		//}else{
+		//	return null;
+		//}
+
+	}
+	public boolean existingTarget(String target){
+		DynamoDBScanExpression scanExpression = new DynamoDBScanExpression();
+
+		Map<String, Condition> scanFilter = new HashMap<String, Condition>();
+
+		// Condition1: DeviceId
+		Condition scanCondition = new Condition()
+				.withComparisonOperator(ComparisonOperator.EQ.toString())
+				.withAttributeValueList(new AttributeValue().withS(target));
+		scanFilter.put("target", scanCondition);
+		scanExpression.setScanFilter(scanFilter);
+
+		List<TS5920Item> results = mapper.scan(TS5920Item.class, scanExpression);
+		if (results.size() > 0){
+			return true;
+		}else{
+			return false;
+		}
+
+	}
+
+        public void putItem(String uid, String target, String desc, String from){
+
+		TS5920Item item = new TS5920Item();
+                long timestamp = (long) (System.currentTimeMillis()/1000);
+		item.setUid(uid);
+		item.setFrom(from);
+		item.setTimestamp(timestamp);
+		item.setTarget(target);
+		item.setDesc(desc);
+			mapper.save(item);
+        }
+
+ @DynamoDBTable(tableName="ts_5920")
+    public static class TS5920Item {
+        private String uid;
+        private long timestamp;
+        private String from;
+        private String desc;
+        private String target;
+
+        @DynamoDBHashKey(attributeName="uid")
+        public String getUid() { return uid; }
+        public void setUid(String uid) { this.uid = uid; }
+
+        @DynamoDBAttribute(attributeName="timestamp")
+        public long getTimestamp() { return timestamp; }
+        public void setTimestamp(long timestamp) { this.timestamp = timestamp; }
+
+        @DynamoDBAttribute(attributeName="from")
+        public String getFrom() { return from; }
+        public void setFrom(String from) { this.from = from; }
+
+        @DynamoDBAttribute(attributeName="desc")
+        public String getDesc() { return desc; }
+        public void setDesc(String desc) { this.desc = desc;}
+
+        @DynamoDBAttribute(attributeName="target")
+        public String getTarget() { return target; }
+        public void setTarget(String target) { this.target = target;}
+
+        @Override
+        public String toString() {
+            return "ts5920 [uid=" + uid + ", from=" + from
+            + ", desc=" + desc + ", target=" + target + "]";
+        }
+    }
+        
+/**
+	public static void main(String argv[]){
+
+   		println("==== dynamodbClient === ");
+
+ 		DynamodbClient dc = new DynamodbClient();
+
+		println(dc.existingTarget("謝小姍"));
+		println(dc.existingTarget("不存在"));
+		println(dc.existingUID("不存在"));
+		println(dc.existingUID("uuid12312312313213"));
+ 	        dc.putItem("uuid12312312313213","謝小姍","我愛你~","老公");
+       
+	}*/
+}
---
+++ app/src/main/java/com/talentservice/ts5920/MainActivity.java
@@ -0,0 +1,232 @@
+package com.talentservice.ts5920;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.StrictMode;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.app.AlertDialog;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    DynamodbClient dc = new DynamodbClient();
+    //String androidId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+
+    public String androidId = "";
+    EditText targetname;
+    EditText desc ;
+    EditText fromname ;
+    TextView theurl ;
+
+    private class Reload5920 extends AsyncTask<String, Integer, Integer> {
+        DynamodbClient.TS5920Item tsItem ;
+
+        protected void onProgressUpdate(Integer... progress) {
+            //setProgressPercent(progress[0]);
+        }
+
+        @Override
+        protected Integer doInBackground(String... params) {
+            if(dc.existingUID(params[0])){
+                tsItem = dc.getItemByUid(params[0]);
+
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Integer result) {
+
+            targetname.setText(tsItem.getTarget());
+            desc.setText(tsItem.getDesc());
+            fromname.setText(tsItem.getFrom());
+            setTargetUrl(tsItem.getTarget());
+        }
+    }
+
+
+    private class Update5920 extends AsyncTask<String, Integer, Boolean> {
+        DynamodbClient.TS5920Item tsItem ;
+
+        protected void onProgressUpdate(Integer... progress) {
+            //setProgressPercent(progress[0]);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            String targetnameString = targetname.getText().toString();
+            String descString = desc.getText().toString();
+            String fromnameString = fromname.getText().toString();
+            dc.putItem(androidId, targetnameString, descString,fromnameString);
+
+            return true;
+        }
+
+        protected void onPostExecute(Boolean result) {
+
+            setTargetUrl(targetname.getText().toString());
+        }
+    }
+    private class Check5920 extends AsyncTask<String, Integer, Boolean> {
+        DynamodbClient.TS5920Item tsItem ;
+
+        protected void onProgressUpdate(Integer... progress) {
+            //setProgressPercent(progress[0]);
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            if(dc.existingTarget(params[0])){
+                //tsItem = dc.getItemByUid(params[0]);
+                return true;
+            }else{
+                return false;
+
+            }
+
+        }
+
+        protected void onPostExecute(Long result) {
+
+
+            setTargetUrl(tsItem.getTarget());
+        }
+    }
+
+
+    private class IfOverwriteFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the Builder class for convenient dialog construction
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setMessage(R.string.dialog_overwrite)
+                    .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // update to new
+                            Update5920 update5920 = new Update5920();
+                            update5920.execute(androidId, null, null);
+                        }
+                    })
+                    .setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // reload to old
+                            Reload5920 asyncUpdateLatest5920 = new Reload5920();
+                            asyncUpdateLatest5920.execute(androidId, null, null);
+                        }
+                    });
+            // Create the AlertDialog object and return it
+            return builder.create();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TelephonyManager tMgr = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+        this.androidId = tMgr.getLine1Number();
+         targetname = ((EditText)findViewById(R.id.edit_target));
+         desc = ((EditText)findViewById(R.id.edit_desc));
+         fromname = ((EditText)findViewById(R.id.edit_from));
+         theurl = (TextView)findViewById(R.id.theurl);
+        //check if the phone number exists
+
+
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        Button okButton = (Button)findViewById(R.id.ok);
+        targetname = ((EditText)findViewById(R.id.edit_target));
+        desc = ((EditText)findViewById(R.id.edit_desc));
+        fromname = ((EditText)findViewById(R.id.edit_from));
+        theurl = (TextView)findViewById(R.id.theurl);
+        okButton.setOnClickListener(this);
+
+
+        Reload5920 asyncUpdateLatest5920 = new Reload5920();
+        asyncUpdateLatest5920.execute(this.androidId, null, null);
+
+
+        return true;
+    }
+    public void setTargetUrl(String targetnameString){
+
+        String targeturl = "http://"+targetnameString+".5920.space";
+        //  theurl.setText(targeturl);
+        theurl.setText(Html.fromHtml("<a href=\"" + targeturl + "\" >" + targeturl + " </a>"));
+        theurl.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case  R.id.ok: {
+
+                String targetnameString = targetname.getText().toString();
+                String descString = desc.getText().toString();
+                String fromnameString = fromname.getText().toString();
+                Check5920 check5920 = new Check5920();
+                try {
+                    if ( !check5920.execute(targetnameString, null, null).get()) {
+
+                        FragmentTransaction ft = getFragmentManager().beginTransaction();
+                        Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+                        if (prev != null) {
+                            ft.remove(prev);
+                        }
+                        ft.addToBackStack(null);
+
+                        // Create and show the dialog.
+                        IfOverwriteFragment ifOverwriteFragment = new IfOverwriteFragment();
+                        ifOverwriteFragment.show(ft, "dialog");
+
+
+                    } else {
+                        Update5920 update5920 = new Update5920();
+                        update5920.execute(this.androidId, null, null);
+                    }
+                }catch(Exception e ){
+                    //alert!!
+                    e.printStackTrace();
+                }
+
+
+                break;
+            }
+            default:
+                break;
+            //.... etc
+        }
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
